c00c750fcd574022c70551002b1e4b91e06f713c
mayank <anandmayank735@gmail.com>
2023-06-21 12:44:47 +0530

    added

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NoteAlt from "@material-ui/icons/NoteAlt";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteAlt />
+          Keeper
+        
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
